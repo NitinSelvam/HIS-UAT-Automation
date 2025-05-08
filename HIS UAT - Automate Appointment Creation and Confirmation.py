b468513e2ec55a4191b6c07bb4cd63e0a61b1517
NitinSelvam <nitin.sel@gmail.com>
2025-05-08 14:39:22 +0530

    Add files via upload

--- HIS UAT - Automate Appointment Creation and Confirmation.py
+++ HIS UAT - Automate Appointment Creation and Confirmation.py
@@ -6,19 +6,19 @@
 # - On HIS UAT, automatically update the appointment slots of all doctors where appointment slots already exist.
 # - For doctors whose appointment slots do not exist, leave them as-is without updating.
 
-# In[6]:
+# In[1]:
 
 
 unit='WF'
 
 
-# In[7]:
+# In[2]:
 
 
 doctor_name = 'Dr. Somashekar S P'
 
 
-# In[8]:
+# In[3]:
 
 
 from selenium.webdriver.common.by import By
@@ -34,9 +34,176 @@ import time
 from datetime import datetime,timedelta
 import pandas as pd,numpy as np
 from selenium.webdriver.firefox.options import Options
+import re
+
+from ordered_set import OrderedSet
+
+
+# In[5]:
+
+
+def is_valid_time_format(t):
+    try:
+        datetime.strptime(t.strip().upper(), "%I:%M %p")
+        return True
+    except ValueError:
+        return False
+
+
+# In[6]:
+
+
+def user_login(driver):
+    userid = driver.find_element("name", "txtUserID")
+    
+    userid.send_keys("Akhil")
+    
+    password = driver.find_element("name", "txtPassword")
+    
+    password.send_keys("Admin@123")
+    
+    # time.sleep(5)
+    
+    login_btn = driver.find_element("name", "btnvalid")
+    login_btn.click()
+    
+    time.sleep(2)
+    
+    if url in driver.current_url.lower():
+        login_btn = driver.find_element("name", "btnvalid")
+        login_btn.click()
+
+
+# In[7]:
+
+
+def click_main_menu(driver):
+    main_menu_element = driver.find_element(By.ID, "RAD_SLIDING_PANE_TEXT_ctl00_rsp1")
+    main_menu_element.click()
+
+
+# In[8]:
+
+
+def click_appointment_element(driver):
+    appointment_element = driver.find_element(
+        By.XPATH,
+        "//a[@href='#'][span/b[normalize-space(text())='Appointment']]"
+    )
+    
+    appointment_element.click()
 
 
 # In[9]:
+
+
+def click_new_appointment_element(driver):
+    appt_new_element = driver.find_element(
+        By.XPATH,
+        "//a[contains(@href, '/Appointment/AppSchedulerV2.aspx')]"
+    )
+    
+    appt_new_element.click()
+
+
+# In[10]:
+
+
+def select_date_pick(driver,date):    
+    date_to_pick = date.strftime("%B %e")
+    
+    date_to_pick = ' '.join([ele for ele in date_to_pick.split(' ') if ele!=''])
+    
+    date_picker_element = driver.find_element(By.XPATH, "//a[@title='{}']".format(date_to_pick))
+    
+    driver.execute_script("arguments[0].click();", date_picker_element)
+
+
+# In[11]:
+
+
+def show_all_provider_checkbox_select(driver):
+    show_all_provider_element = driver.find_element("id", "ctl00_ContentPlaceHolder1_chkShowAllProviders")
+    
+    is_checked = driver.execute_script("return arguments[0].checked;", show_all_provider_element)
+    
+    if is_checked==False:
+        driver.execute_script("arguments[0].click();", show_all_provider_element)
+        time.sleep(1)
+
+
+# In[23]:
+
+
+def select_doctor_element_from_all_doctors_list(driver,doctor_name):
+    doctor_list_element = driver.find_element("id", "ctl00_ContentPlaceHolder1_RadLstDoctor_Arrow")
+    
+    doctor_list_element.click()
+    
+    html_list = driver.find_element(By.CLASS_NAME, "rcbList")
+    items = html_list.find_elements(By.TAG_NAME,"li")
+
+    # print(len([item.text for item in items]))
+    doctorname_ele = [item for item in items if item.text == doctor_name][0]
+    
+    doctorname_ele.click()
+    time.sleep(3)
+
+
+# In[13]:
+
+
+def get_list_of_all_timeslots(driver):
+    slots_scheduler_element = driver.find_element(By.CLASS_NAME, "rsContentTable")
+    
+    temp_timeslots_list = []
+    temp_sub_elements_list = []
+    
+    # Step 1: Locate the scrollable container
+    scroll_container = driver.find_element(By.CLASS_NAME, "rsContentScrollArea")
+    
+    height = driver.execute_script("return arguments[0].style.height;", scroll_container)
+    height_in_px = height.strip('px')
+    
+    # Step 2: Scroll gradually until the bottom is reached
+    last_scroll_top = -1
+    while True:
+        # Check if we've reached the bottom
+        current_scroll_top = driver.execute_script("return arguments[0].scrollTop;", scroll_container)
+        if current_scroll_top == last_scroll_top:
+            break
+        last_scroll_top = current_scroll_top
+    
+        timeslots_time_ele = driver.find_element(By.CLASS_NAME, "rsVerticalHeaderTable")
+        timeslot_elements = timeslots_time_ele.find_elements(By.TAG_NAME, "tr")
+    
+        slots_scheduler_element = driver.find_element(By.CLASS_NAME, "rsContentTable")
+        sub_elements = slots_scheduler_element.find_elements(By.TAG_NAME, "tr")
+        
+        inner_list = [ele.text for ele in timeslot_elements if ele.text!='']
+        # print(inner_list)
+    
+        temp_sub_elements_list.append(sub_elements)
+        
+        temp_timeslots_list.append(inner_list)
+    
+        # Scroll down a bit
+        driver.execute_script("arguments[0].scrollTop += {};".format(height_in_px), scroll_container)
+        time.sleep(0.2)  # small wait for content to load/render
+
+    sub_elements_list = [inner for outer in temp_sub_elements_list for inner in outer]
+    sub_elements_list = list(OrderedSet(sub_elements_list))
+    sub_elements_list = [sub_ele for sub_ele in sub_elements_list if sub_ele.text.strip()=='']
+    
+    all_timeslots_list = [inner for outer in temp_timeslots_list for inner in outer]
+    all_timeslots_list = list(np.unique(all_timeslots_list))
+    all_timeslots_list = [t for t in all_timeslots_list if is_valid_time_format(t)]
+    all_timeslots_list = sorted(all_timeslots_list, key=lambda x: datetime.strptime(x, '%I:%M %p'))
+    
+    return sub_elements_list,all_timeslots_list
+
+
+# In[14]:
 
 
 firefox_options = Options()
@@ -56,30 +223,13 @@ url = '{}/login'.format(base_url)
 driver.get(url)
 
 
-# In[10]:
+# In[15]:
 
 
-userid = driver.find_element("name", "txtUserID")
-
-userid.send_keys("Akhil")
-
-password = driver.find_element("name", "txtPassword")
-
-password.send_keys("Admin@123")
-
-# time.sleep(5)
-
-login_btn = driver.find_element("name", "btnvalid")
-login_btn.click()
-
-time.sleep(2)
-
-if url in driver.current_url.lower():
-    login_btn = driver.find_element("name", "btnvalid")
-    login_btn.click()
+user_login(driver)
 
 
-# In[11]:
+# In[16]:
 
 
 already_logged_in_ok_button = driver.find_elements("name", "ButtonOk")
@@ -88,170 +238,83 @@ if already_logged_in_ok_button!=[] and already_logged_in_ok_button[0].get_proper
     already_logged_in_ok_button[0].click()
 
 
-# In[12]:
+# In[17]:
 
 
-main_menu_element = driver.find_element(By.ID, "RAD_SLIDING_PANE_TEXT_ctl00_rsp1")
+click_main_menu(driver)
 
-main_menu_element.click()
 
+# In[18]:
 
-# In[13]:
 
+click_appointment_element(driver)
 
-appointment_element = driver.find_element(
-    By.XPATH,
-    "//a[@href='#'][span/b[normalize-space(text())='Appointment']]"
-)
 
-appointment_element.click()
+# In[37]:
 
 
-# In[25]:
+click_new_appointment_element(driver)
 
 
-appt_new_element = driver.find_element(
-    By.XPATH,
-    "//a[contains(@href, '/Appointment/AppSchedulerV2.aspx')]"
-)
+# In[38]:
 
-appt_new_element.click()
 
+date = datetime.now() + timedelta(days=1)  # Select tomorrow's date
 
-# In[26]:
+select_date_pick(driver,date)
 
 
-date = datetime.now() + timedelta(days=1)
+# In[39]:
 
-date_to_pick = date.strftime("%B %e")
 
-date_to_pick = ' '.join([ele for ele in date_to_pick.split(' ') if ele!=''])
+show_all_provider_checkbox_select(driver)
 
-date_to_pick
 
-# month = date.strftime("%B")
-# year = date.strftime("%Y")
-# date_day = date.strftime("%d")
+# In[40]:
 
-# print(date_to_pick,'--',month,'--',year,'--',date_day)
 
+select_doctor_element_from_all_doctors_list(driver,doctor_name)
 
-# In[27]:
 
+# In[41]:
 
-date_picker_element = driver.find_element(By.XPATH, "//a[@title='{}']".format(date_to_pick))
 
-driver.execute_script("arguments[0].click();", date_picker_element)
+sub_elements_list,all_timeslots_list = get_list_of_all_timeslots(driver)
 
 
-# In[28]:
+# In[42]:
 
 
-show_all_provider_element = driver.find_element("id", "ctl00_ContentPlaceHolder1_chkShowAllProviders")
+print(len(sub_elements_list))
+print(len(all_timeslots_list))
 
-# show_all_provider_element.click()
 
-driver.execute_script("arguments[0].click();", show_all_provider_element)
-time.sleep(1)
+# In[43]:
 
 
-# In[29]:
+index=0
 
+while(True):
+    if index==10:
+        break
+    if index>=1:
+        driver.switch_to.parent_frame()
+        click_new_appointment_element(driver)
+        time.sleep(1)
+        date = datetime.now() + timedelta(days=1)  # Select tomorrow's date
+        select_date_pick(driver,date)
+        time.sleep(1)
+        show_all_provider_checkbox_select(driver)
+        select_doctor_element_from_all_doctors_list(driver,doctor_name)
+        sub_elements_list,all_timeslots_list = get_list_of_all_timeslots(driver)
 
-doctor_list_element = driver.find_element("id", "ctl00_ContentPlaceHolder1_RadLstDoctor_Arrow")
+    try:
+        sub_elements_list[0].click()
 
-doctor_list_element.click()
-
-html_list = driver.find_element(By.CLASS_NAME, "rcbList")
-items = html_list.find_elements(By.TAG_NAME,"li")
-
-doctorname_ele = [item for item in items if item.text == doctor_name][0]
-
-doctorname_ele.click()
-time.sleep(2)
-
-
-# In[30]:
-
-
-slots_scheduler_element = driver.find_element(By.CLASS_NAME, "rsContentTable")
-
-
-# In[31]:
-
-
-slot_element_list = slots_scheduler_element.find_elements(By.TAG_NAME, "tr")
-
-len(slot_element_list)
-
-
-# In[32]:
-
-
-timeslots_time_ele = driver.find_element(By.CLASS_NAME, "rsVerticalHeaderTable")
-elements = timeslots_time_ele.find_elements(By.TAG_NAME, "tr")
-
-len(elements)
-
-
-# In[33]:
-
-
-timeslot_time_list = [ele.text for ele in elements if ele.text!='']
-timeslot_time_list
-
-
-# In[34]:
-
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
-
-# Locate the scrollable element (change locator accordingly)
-scrollable = slots_scheduler_element
-
-# Set scroll parameters
-scroll_pause_time = 0.3  # seconds
-scroll_step = 100        # pixels per step
-
-# Get initial scroll position and height
-last_scroll_position = 0
-max_scroll_reached = False
-
-while not max_scroll_reached:
-    # Scroll down by scroll_step
-    driver.execute_script("arguments[0].scrollTop += arguments[1];", scrollable, scroll_step)
-    time.sleep(scroll_pause_time)
-
-    # Get new scroll position
-    current_position = driver.execute_script("return arguments[0].scrollTop;", scrollable)
-    total_height = driver.execute_script("return arguments[0].scrollHeight;", scrollable)
-    client_height = driver.execute_script("return arguments[0].clientHeight;", scrollable)
-
-    # Debug output (optional)
-    print(f"Scroll position: {current_position}/{total_height}")
-
-    # If we've reached the bottom, stop
-    if current_position + client_height >= total_height:
-        max_scroll_reached = True
-    elif current_position == last_scroll_position:
-        # Nothing is loading anymore, break the loop
-        max_scroll_reached = True
-
-    last_scroll_position = current_position
-
-# Once done scrolling, get all sub-elements inside the scrollable area
-sub_elements = scrollable.find_elements(By.TAG_NAME, "tr")
-
-print(f"Captured {len(sub_elements)} sub-elements")
-
-
-# In[35]:
-
-
-for index in range(len(sub_elements)):
-    sub_elements[index].click()
+    except IndexError as e:
+        print("################################################")
+        print("All appointments booked")
+        break
 
     new_appt_select_element = driver.find_element(
         By.XPATH,
@@ -295,73 +358,39 @@ for index in range(len(sub_elements)):
     # make_appointment_button = driver.find_element(By.ID, "btnsave")
     # make_appointment_button.click()
 
-    break
+    # break
 
-
-# In[36]:
-
-
-patient_table_list_ele = driver.find_elements(
-By.XPATH,
-"//tr[contains(@id, 'gvEncounter_ctl00__')]"
-)
-
-select_patient_link = [ele.find_element(By.TAG_NAME, "a") for ele in patient_table_list_ele][index]
-
-select_patient_link.click()
-driver.switch_to.parent_frame()
-time.sleep(3)
-mobile_no_ele = driver.find_element(By.ID,'txtMobile')
-alternative_mobile_no_ele = driver.find_element(By.ID,'txtAlterNetMobileNo')
-mobile_no = mobile_no_ele.get_attribute('value')
-driver.execute_script("arguments[0].setAttribute('value',arguments[1])",alternative_mobile_no_ele, mobile_no)
-
-referred_type_list_ele = driver.find_element(By.ID,'dropReferredType_Input')
-referred_type_list_ele.click()
-time.sleep(1)
-
-dropdown_ele = driver.find_element(By.ID, "dropReferredType_DropDown")
-html_list = dropdown_ele.find_element(By.CLASS_NAME, "rcbList")
-items = html_list.find_elements(By.TAG_NAME,"li")
-
-self_referred_type_ele = [item for item in items if item.text == 'Self'][0]
-self_referred_type_ele.click()
-
-time.sleep(2)
-
-make_appointment_button = driver.find_element(By.ID, "btnsave")
-make_appointment_button.click()
-
-
-
-# In[38]:
-
-
-# appt_created_times = []
-
-# appt_created_times.append(timeslot_time_list[index])
-
-
-# In[40]:
-
-
-appt_created_times
-
-
-# In[ ]:
-
-
-driver.switch_to.default_content()
-
-
-# In[ ]:
-
-
-sub_elements[index].click()
-
-
-# In[ ]:
-
-
-
+    patient_table_list_ele = driver.find_elements(
+    By.XPATH,
+    "//tr[contains(@id, 'gvEncounter_ctl00__')]"
+    )
+    
+    select_patient_link = [ele.find_element(By.TAG_NAME, "a") for ele in patient_table_list_ele][index]
+    
+    select_patient_link.click()
+    driver.switch_to.parent_frame()
+    time.sleep(3)
+    mobile_no_ele = driver.find_element(By.ID,'txtMobile')
+    alternative_mobile_no_ele = driver.find_element(By.ID,'txtAlterNetMobileNo')
+    mobile_no = mobile_no_ele.get_attribute('value')
+    driver.execute_script("arguments[0].setAttribute('value',arguments[1])",alternative_mobile_no_ele, mobile_no)
+    
+    referred_type_list_ele = driver.find_element(By.ID,'dropReferredType_Input')
+    referred_type_list_ele.click()
+    time.sleep(1)
+    
+    dropdown_ele = driver.find_element(By.ID, "dropReferredType_DropDown")
+    html_list = dropdown_ele.find_element(By.CLASS_NAME, "rcbList")
+    items = html_list.find_elements(By.TAG_NAME,"li")
+    
+    self_referred_type_ele = [item for item in items if item.text == 'Self'][0]
+    self_referred_type_ele.click()
+    
+    time.sleep(2)
+    
+    make_appointment_button = driver.find_element(By.ID, "btnsave")
+    make_appointment_button.click()
+    time.sleep(2)
+    index+=1
+    # break
 
